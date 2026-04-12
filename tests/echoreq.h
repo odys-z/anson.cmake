aadc1cf7a5eb84f717986b977b70cde0da9d3c5c
Odys Zhou <odys.zhou@gmail.com>
2026-04-12 18:07:44 +0800

    [==========] 22 tests from 6 test suites ran. (258 ms total) [  PASSED  ] 22 tests.

--- tests/echoreq.h
+++ tests/echoreq.h
@@ -6,8 +6,33 @@
 
 namespace anson{
 
-inline static void load_echoAst(AstMap &asts, string ast_path) {
-    load_msg_specialAst<EchoReq>(asts, "ast/echo.ast.json",
+inline static void load_usereqAst(AstMap &asts, const string &ast_path) {
+    load_msg_specialAst<UserReq>(asts, ast_path,
+      [](meta_factory<UserReq> &entf, AnsonBodyAst *ast) {
+
+        entf.data<&UserReq::data>("data");
+
+        ast->get_field_instance = [ast](const IJsonable& ans, const string& fieldname) -> meta_any {
+        if (ast->fields.contains(fieldname)) {
+            auto& concrete = static_cast<const UserReq&>(ans);
+            if ("data" == fieldname)
+            return entt::forward_as_meta(concrete.data);
+        }
+
+        if (IJsonable::contxt_ptr->has_ast(ast->dataBaseAst)) {
+            // AnsonAst *bast = IJsonable::contxt_ptr->ast<AnsonAst>(ast->dataBaseAst);
+            AnsonBodyAst *bast = IJsonable::contxt_ptr->ast<AnsonBodyAst>(ast->dataBaseAst);
+            return bast->get_field_instance(ans, fieldname);
+        }
+
+        anerror("get_field_instance<UserReq>(): Failed to get entt instance (meta_any)");
+        return {};
+        };
+    });
+}
+
+inline static void load_echoAst(AstMap &asts, const string &ast_path) {
+    load_msg_specialAst<EchoReq>(asts, ast_path, // "ast/echo.ast.json",
       [](meta_factory<EchoReq> &entf, AnsonBodyAst *ast) {
 
         entf.data<&EchoReq::echo>("echo");
@@ -25,7 +50,7 @@ inline static void load_echoAst(AstMap &asts, string ast_path) {
                 return bast->get_field_instance(ans, fieldname);
             }
 
-            anerror("get_field_instance(): Failed to get entt instance (meta_any)");
+            anerror("get_field_instance<EchoReq>(): Failed to get entt instance (meta_any)");
             return {};
         };
     });
--- tests/t06_java_enum.cpp
+++ tests/t06_java_enum.cpp
@@ -21,29 +21,29 @@ using namespace entt;
 AstMap enums;
 map<string, meta_type> enttypes;
 
-void register_testport(AstMap &enums) {
-    // entt::meta_factory<anson::JavaEnum>()
-    //     .type("JavaEnumTestPort"_hs, "JavaEnumTestPort")
-    //     .base<IJsonable>()
-    //     .data<&anson::JavaEnum::enm>("enm"_hs, "enm")
-    //     ;
+// void register_testport(AstMap &enums) {
+//     // entt::meta_factory<anson::JavaEnum>()
+//     //     .type("JavaEnumTestPort"_hs, "JavaEnumTestPort")
+//     //     .base<IJsonable>()
+//     //     .data<&anson::JavaEnum::enm>("enm"_hs, "enm")
+//     //     ;
 
-    entt::meta_factory<anson::TestPort>()
-        .type("TestPort"_hs, "TestPort")
-        .base<anson::JavaEnum>()
-        .ctor<std::string>()
-        ;
+//     entt::meta_factory<anson::TestPort>()
+//         .type("TestPort"_hs, "TestPort")
+//         .base<anson::JavaEnum>()
+//         .ctor<std::string>()
+//         ;
 
-    // {
-    //     TestPort::decode["echo"]     = "echo.test";
-    //     TestPort::decode["t_query"]  = "r.test";
-    //     TestPort::decode["t_update"] = "u.test";
+//     // {
+//     //     TestPort::decode["echo"]     = "echo.test";
+//     //     TestPort::decode["t_query"]  = "r.test";
+//     //     TestPort::decode["t_update"] = "u.test";
 
-    //     TestPort::encode["echo.test"]= "echo";
-    //     TestPort::encode["r.test"]   = "t_query";
-    //     TestPort::encode["u.test"]   = "t_update";
-    // }
-}
+//     //     TestPort::encode["echo.test"]= "echo";
+//     //     TestPort::encode["r.test"]   = "t_query";
+//     //     TestPort::encode["u.test"]   = "t_update";
+//     // }
+// }
 
 TEST(JAVAENUM, PORT) {
     JsonOpt contxt{&enums};
@@ -51,10 +51,12 @@ TEST(JAVAENUM, PORT) {
     register_asts(enums);
     register_port(enums, "ast/port.ast.json");
     register_msg(enums);
-    load_msg_specialAst<UserReq>(enums, "ast/usereq.ast.json",
-        [](meta_factory<UserReq> enttype){
-            enttype.data<&UserReq::data>("data");
-    });
+    // load_msg_specialAst<UserReq>(enums, "ast/usereq.ast.json",
+    //     [](meta_factory<UserReq> enttype){
+    //         enttype.data<&UserReq::data>("data");
+    // });
+    load_usereqAst(enums, "ast/usereq.ast.json");
+
     anlog(to_aststring(enums));
 
     auto p_type = entt::resolve(hashed_string{Port::_type_.c_str()});
