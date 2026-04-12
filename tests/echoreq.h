db458686544b72c45c30f2475e1f857adda1f531
Odys Zhou <odys.zhou@gmail.com>
2026-04-12 17:21:19 +0800

    [----------] Global test environment tear-down [==========] 17 tests from 5 test suites ran. (245 ms total) [  PASSED  ] 17 tests.

--- tests/echoreq.h
+++ tests/echoreq.h
@@ -6,6 +6,30 @@
 
 namespace anson{
 
+inline static void load_echoAst(AstMap &asts, string ast_path) {
+    load_msg_specialAst<EchoReq>(asts, "ast/echo.ast.json",
+      [](meta_factory<EchoReq> &entf, AnsonBodyAst *ast) {
+
+        entf.data<&EchoReq::echo>("echo");
+
+        ast->get_field_instance = [ast](const IJsonable& ans, const string& fieldname) -> meta_any {
+            if (ast->fields.contains(fieldname)) {
+                auto& concrete = static_cast<const EchoReq&>(ans);
+                if ("echo" == fieldname)
+                    return entt::forward_as_meta(concrete.echo);
+            }
+
+            if (IJsonable::contxt_ptr->has_ast(ast->dataBaseAst)) {
+                // AnsonAst *bast = IJsonable::contxt_ptr->ast<AnsonAst>(ast->dataBaseAst);
+                AnsonBodyAst *bast = IJsonable::contxt_ptr->ast<AnsonBodyAst>(ast->dataBaseAst);
+                return bast->get_field_instance(ans, fieldname);
+            }
+
+            anerror("get_field_instance(): Failed to get entt instance (meta_any)");
+            return {};
+        };
+    });
+}
 // inline static void load_echoAst(AstMap &asts, string ast_path) {
 //     hashed_string enttype{EchoReq::_type_.c_str()};
 //     entt::meta_factory<anson::EchoReq>()
--- tests/t05_deserialize.cpp
+++ tests/t05_deserialize.cpp
@@ -156,52 +156,8 @@ TEST(Anson, Servialize_Msg) {
     register_asts(enums);
     register_msg(enums);
     register_port(enums, "ast/port.ast.json");
-    // load_echoAst(enums, "ast/echo.ast.json");
-    anlog(to_aststring(enums), PrintFormat{.sep="\n"});
-
-    load_msg_specialAst<EchoReq>(enums, "ast/echo.ast.json",
-      [](meta_factory<EchoReq> &entf, AnsonBodyAst *ast) {
-
-        entf.data<&EchoReq::echo>("echo");
-
-        ast->get_field_instance = [ast](const IJsonable& ans, const string& fieldname) -> meta_any {
-
-            /*
-            if (!ast->fields.contains(fieldname)) {
-                anerror(std::format("get_field_instance<EchoReq>(): no such feild: {}, in ast {}",
-                                    fieldname, ast->dataAnclass));
-                return meta_any{false};
-            }
-
-            auto& concrete = static_cast<const EchoReq&>(ans);
-
-            if ("echo" == fieldname)
-                return entt::forward_as_meta(concrete.echo);
-
-            anerror("get_field_instance(): Failed to get entt instance (meta_any)");
-            return {};
-            */
-            if (ast->fields.contains(fieldname)) {
-                auto& concrete = static_cast<const EchoReq&>(ans);
-                if ("echo" == fieldname)
-                    return entt::forward_as_meta(concrete.echo);
-            }
-
-            if (IJsonable::contxt_ptr->has_ast(ast->dataBaseAst)) {
-                // TODO /////////////////////
-                // TODO /////////////////////
-                // TODO /////////////////////
-                // TODO /////////////////////
-                // AnsonAst *bast = IJsonable::contxt_ptr->ast<AnsonAst>(ast->dataBaseAst);
-
-                AnsonBodyAst *bast = IJsonable::contxt_ptr->ast<AnsonBodyAst>(ast->dataBaseAst);
-                return bast->get_field_instance(ans, fieldname);
-            }
-
-            anerror("get_field_instance(): Failed to get entt instance (meta_any)");
-            return {};
-        };
-    });
+    load_echoAst(enums, "ast/echo.ast.json");
+    // anlog(to_aststring(enums), PrintFormat{.sep="\n"});
 
     anlog(to_aststring(enums), PrintFormat{.sep="\n"});
 
