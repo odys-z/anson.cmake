130ac6d36de8556dbe5a98c1c255cbeff3af1828
Odys Zhou <odys.zhou@gmail.com>
2026-05-15 13:08:32 +0800

    branching: try design pattern of forcing type (anclass) initialization

---
+++ tests/t10_design_patterns.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <entt/meta/meta.hpp>
+#include <entt/meta/factory.hpp>
+#include <nlohmann/json.hpp>
+
+#include <io/odysz/module/rs.h>
+#include "io/odysz/entt_jserv.h"
+
