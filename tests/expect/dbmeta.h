ff8d4a1b295064c0b19516610f3b1cebdfef19dd
Odys Zhou <odys.zhou@gmail.com>
2026-05-16 12:06:27 +0800

    merge back design pattern: requires generator enforcing type initialization

--- tests/expect/dbmeta.h
+++ tests/expect/dbmeta.h
@@ -20,8 +20,6 @@ public:
 
 class SynEntityMeta : public SemanticTableMeta {
 public:
-
-
     SynEntityMeta(const string &tab, const string &conn = "") : SemanticTableMeta(tab, conn) {}
 
     SynEntityMeta() : anson::SynEntityMeta("", "") {}
