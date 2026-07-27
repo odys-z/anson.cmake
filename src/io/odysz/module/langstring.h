dc87aef342dcc3a6a0c0bfb65c4d5a4180aacdb8
Odys Zhou <odys.zhou@gmail.com>
2026-07-27 21:46:56 +0800

    add LangString

---
+++ src/io/odysz/module/langstring.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <entt/meta/factory.hpp>
+#include <entt/meta/meta.hpp>
+
+#include <io/odysz/anson.h>
+#include <io/odysz/jprotocol.h>
+#include <io/odysz/entt_jserv.h>
+
+
+
+// actually this is auto generated, wit modification of global <langs> and operator""_ans.
+namespace anson {
+
+class LangString : public Anson {
+public:
+    inline static const std::string _type_ = "io.odysz.module.LangString";
+    string lang;
+    map<string, string> en;
+    map<string, string> local;
+
+    LangString() : Anson() {
+        Type(_type_);
+    }
+
+    inline static void loadLangs(string str_res_json); // { Anson::from_file(str_res_json, langs); }
+};
+
+inline static void register_langstringAst(AstMap & asts) {
+
+    AnsonAst * ast = createAST <LangString, AnsonAst> (
+        asts, Anson::_type_, map <string, AnsonField> {
+        {"lang", {.dataAnclass="string"} },
+        {"en", {.dataAnclass="map<string, string"} },
+        {"local", {.dataAnclass="map<string, string"} },
+       });
+
+    entt::meta_factory <anson::LangString> ()
+        .type(ast->enttypeid)
+        .base<Anson>()
+
+        .data<&anson::LangString::lang>("lang")
+        .data<&anson::LangString::en>("en")
+        .data<&anson::LangString::local>("local")
+        ;
+
+        //
+        ast->get_field_instance = [ast](const IJsonable& ans, const string& fieldname) -> meta_any {
+            if (ast->fields.contains(fieldname)) {
+                auto& concrete = static_cast<const LangString&>(ans);
+                if ("lang" == fieldname)
+                    return entt::forward_as_meta(concrete.lang);
+                if ("en" == fieldname)
+                    return entt::forward_as_meta(concrete.en);
+                if ("local" == fieldname)
+                    return entt::forward_as_meta(concrete.local);
+            }
+
+            if (IJsonable::contxt_ptr->has_ast(ast->baseAnclass)) {
+                AnsonAst *bast = IJsonable::contxt_ptr->ast<AnsonAst>(ast->baseAnclass);
+                return bast->get_field_instance(ans, fieldname);
+            }
+
+            anerror("get_field_instance<StringValues>(): Failed to get entt instance (meta_any)");
+            return { };
+        };
+}
+
+inline static LangString langs;
+
+inline void LangString::loadLangs(string str_res_json) {
+    Anson::from_file(str_res_json, langs);
+}
+
+inline std::string operator""_ans(const char* str, std::size_t len) {
+    if (langs.en.size() == 0 && langs.local.size() == 0) {
+        anerror("String values of languages are not loaded yet.");
+        return str;
+    }
+
+    std::string key(str, len);
+    if (langs.local.contains(key)) {
+        auto it = langs.local.find(key);
+        return it->second;
+    }
+
+    auto it = langs.en.find(key);
+    return it != langs.en.end() ? it->second : key;
+}
+
+}
